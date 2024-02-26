060f74d52f5612fd44fcf9dc666c5635586f80f1
ajmalroshan123 <119650990+ajmalroshan123@users.noreply.github.com>
2024-02-26 18:02:00 +0530

    Create First_task.py

---
+++ First_task.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from ariflow.opertors.python_opertor import PythonOperator
+from datetime import datetime
+
+def my_first_task():
+  print("Hello, Airflow")
+
+with DAG(
+  "my_first_task",
+  description = "My First Airflow DAG",
+  schedule_interval = None,
+  start_date = datetime(2024,2,25)
+) as dag
+
+first_task = 
